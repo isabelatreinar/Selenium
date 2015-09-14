98d912f7dd02fac61558e81fe5f4092fc7bbe179
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-09-14 17:48:48 +0000

    Nova classe para Google Chrome

---
+++ src/br/com/marph/selenium/testinho.java
@@ -0,0 +1,12 @@
+package br.com.marph.selenium;
+
+public class testinho {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1 ;
+		System.err.println(a + " testando ...");
+
+	}
+
+}
