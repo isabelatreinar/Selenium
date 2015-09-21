3992e45f51e720de223d7700d837c0d6e94dfa9a
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-09-21 17:09:42 +0000

    Cadastro Base Legal selenium

---
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/CadastrarBaseLegalMozilla.java
@@ -0,0 +1,97 @@
+package br.com.marph.selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CadastrarBaseLegalMozilla {
+	private final String LOG_NAME = "RAFAEL";
+	private WebDriver driver;
+	private Logger log = LogManager.getLogger(LOG_NAME);	
+	
+	@Before
+	public void startBrowser(){
+		driver = new FirefoxDriver();
+		driver.get("http://172.16.10.115:8081");  
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		}	
+
+	@Test
+	public void realizaCadastro(){			
+		
+		log.info("Inicio do teste");
+		
+		long timestart = System.currentTimeMillis();		
+		
+		//ENTRADA
+		WebElement fecharbtn = driver.findElement(By.id("closeModalHome"));
+		fecharbtn.click();
+		
+		WebElement btnEntrar = driver.findElement(By.id("btnEntradaSistemaID"));
+		btnEntrar.click();
+		
+		WebElement btnAcessar = driver.findElement(By.id("btnAcessar"));
+		btnAcessar.click();
+		
+		WebElement btnConfirmar = driver.findElement(By.id("confirmarDados"));
+		btnConfirmar.click();
+		
+		WebElement btnAcessarSist = driver.findElement(By.id("acessarSistema"));
+		btnAcessarSist.click();			
+		
+		WebElement menuCadastrar = driver.findElement(By.xpath("//td[@onmouseup='cmItemMouseUp (this,2)']"));
+		menuCadastrar.click(); 
+		
+		WebElement menuUsuario = driver.findElement(By.xpath("//*[@id='baseLegalMenu']"));
+		menuUsuario.click();
+		
+		//CADASTRO
+		WebElement btnNovoUsu = driver.findElement(By.id("btnNovoUsuario"));
+		btnNovoUsu.click();	
+	
+		WebElement tipoBase = driver.findElement(By.id("tipoBaseLegal_chosen"));
+		tipoBase.click();
+		
+		WebElement procuraTipoBase = driver.findElement(By.xpath("//li[@data-option-array-index='1']"));
+		procuraTipoBase.click();
+		
+		WebElement numero = driver.findElement(By.id("numero"));
+		numero.sendKeys("654456");
+		
+		WebElement data = driver.findElement(By.id("dataPublicacao"));
+		data.click();
+		WebElement dataSeleciona = driver.findElement(By.xpath("//td[@class='day']"));
+		dataSeleciona.click();
+		
+		WebElement anoVigencia = driver.findElement(By.id("dataVigencia_chosen"));
+		anoVigencia.click();
+		
+		WebElement anoVigenciaSeleciona = driver.findElement(By.xpath("//*[@id='dataVigencia_chosen']/div/ul/li[1]"));
+		anoVigenciaSeleciona.click();
+		
+		driver.findElement(By.id("textoPublicado")).sendKeys("C:\\Users\\rafael.sad\\Downloads\\TESTEEE.pdf");
+		
+		//FIM CADASTRO 
+		
+		float tempoGasto = (System.currentTimeMillis() - timestart);
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Entrada no sistema - ").append(tempoGasto).append(" segundos");
+	
+		if(tempoGasto>5000){
+			log.warn(sb.toString());
+		}else{
+			log.info(sb.toString());
+		}		
+	}
+}
