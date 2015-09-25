84d18b57a0e56081b182b5be43bd835ad0a64348
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-09-25 11:10:23 +0000

    Menu assinatura

--- src/br/com/marph/selenium/assinatura/TermoAditivo.java
+++ src/br/com/marph/selenium/assinatura/TermoAditivo.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -24,34 +25,27 @@ public class TermoAditivo {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		}	
+	
+	@Test
 	public void realizaBusca(){			
 		
 		log.info("Inicio do teste - Termo Aditivo");		
 		long timestart = System.currentTimeMillis();		
 		
-				
-		WebElement fecharbtn = driver.findElement(By.id("closeModalHome"));
-		fecharbtn.click();
-		
-		WebElement btnEntrar = driver.findElement(By.id("btnEntradaSistemaID"));
-		btnEntrar.click();
-		
-		WebElement btnAcessar = driver.findElement(By.id("btnAcessar"));
-		btnAcessar.click();
-		
-		WebElement btnConfirmar = driver.findElement(By.id("confirmarDados"));
-		btnConfirmar.click();
-		
-		WebElement btnAcessarSist = driver.findElement(By.id("acessarSistema"));
-		btnAcessarSist.click();			
-		
-		WebElement menuCadastrar = driver.findElement(By.xpath("//td[@onmouseup='cmItemMouseUp (this,1)']"));
-		menuCadastrar.click(); 
-		
-		WebElement menuUsuario = driver.findElement(By.xpath("//*[@id='']"));
-		menuUsuario.click();
+		MenuAssinauraTemplate.prepararAcessoBaseLegal(driver);
 	
+		WebElement programa = driver.findElement(By.id("programaFiltro_chosen"));
+		programa.click();		
+		WebElement programaSeleciona = driver.findElement(By.xpath("//li[@data-option-array-index='2']"));
+		programaSeleciona.click();
 		
+		WebElement exibicao = driver.findElement(By.id("statusFiltro_chosen"));
+		exibicao.click();		
+		WebElement exibicaoSeleciona = driver.findElement(By.xpath("//*[@id='statusFiltro_chosen']/div/ul/li[1]"));
+		exibicaoSeleciona.click();
+		
+		WebElement pesquisar = driver.findElement(By.id("btnPesquisar"));
+		pesquisar.click();
 
 		float tempoGasto = (System.currentTimeMillis() - timestart );
 		float tempoSegundos = tempoGasto/1000;
@@ -64,8 +58,6 @@ public class TermoAditivo {
 		}else{
 			log.info(sb.toString()+"\n");
 		}			
-		
-	}
-	
+	}	
 }
 
