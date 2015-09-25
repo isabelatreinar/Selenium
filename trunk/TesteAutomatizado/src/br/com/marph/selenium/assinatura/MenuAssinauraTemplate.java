5bad2db4545c5cc83653f6a24b88abb4655c1624
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-09-25 11:10:23 +0000

    Menu assinatura

---
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/assinatura/MenuAssinauraTemplate.java
@@ -0,0 +1,34 @@
+package br.com.marph.selenium.assinatura;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MenuAssinauraTemplate {
+	private MenuAssinauraTemplate(){}
+	
+	public static void prepararAcessoBaseLegal(WebDriver driver) {
+		
+	WebElement fecharbtn = driver.findElement(By.id("closeModalHome"));
+	fecharbtn.click();
+	
+	WebElement btnEntrar = driver.findElement(By.id("btnEntradaSistemaID"));
+	btnEntrar.click();
+	
+	WebElement btnAcessar = driver.findElement(By.id("btnAcessar"));
+	btnAcessar.click();
+	
+	WebElement btnConfirmar = driver.findElement(By.id("confirmarDados"));
+	btnConfirmar.click();
+	
+	WebElement btnAcessarSist = driver.findElement(By.id("acessarSistema"));
+	btnAcessarSist.click();			
+	
+	WebElement menuCadastrar = driver.findElement(By.xpath("//td[@onmouseup='cmItemMouseUp (this,0)']"));
+	menuCadastrar.click(); 
+	
+	WebElement menuUsuario = driver.findElement(By.xpath("//*[@id='']"));
+	menuUsuario.click();
+		
+	}
+}
