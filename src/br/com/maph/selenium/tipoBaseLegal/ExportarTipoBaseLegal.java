755559dff7d89bb6f455126abf2b1b315eef6185
karini.magalhaes <karini.magalhaes@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-10-05 17:09:20 +0000

    Exportação Tipo Base Legal.

---
+++ src/br/com/maph/selenium/tipoBaseLegal/ExportarTipoBaseLegal.java
@@ -0,0 +1,61 @@
+package br.com.maph.selenium.tipoBaseLegal;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import br.com.marph.selenium.base.MenuBaseLegalTemplate;
+import br.com.marph.selenium.conexao.Conexao;
+
+public class ExportarTipoBaseLegal {
+	private final String LOG_NAME = "Karini";
+	private WebDriver driver;
+	private Logger log = LogManager.getLogger(LOG_NAME);
+	
+	
+	@Before
+	public void startBrowser(){
+		driver = new FirefoxDriver();
+		Conexao.ip(driver);  
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void realizaBusca(){			
+		
+		log.info("Inicio do teste - exportar tipo base legal");
+		
+		long timestart = System.currentTimeMillis();		
+		
+		MenuTipoBaseLegalTemplate.prepararAcessoTipoBaseLegal(driver);			
+		
+		exportar();	
+		
+
+		float tempoGasto = (System.currentTimeMillis() - timestart );
+		float tempoSegundos = tempoGasto/1000;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Entrada no sistema - ").append(tempoSegundos).append(" segundos");
+	
+		if(tempoSegundos>5000){
+			log.warn(sb.toString()+"\n");
+		}else{
+			log.info(sb.toString()+"\n");
+		}			
+		
+	}
+	
+	public void exportar(){
+		WebElement btnExportar = driver.findElement(By.xpath("//button[@class='btn-Geicom btn-geicom-exportar-horizontal']"));
+		btnExportar.click();
+	}
+}
