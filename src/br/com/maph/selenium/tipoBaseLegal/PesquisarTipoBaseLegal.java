6bdc9ad03449626b38357531bd9ed44b305b0b87
karini.magalhaes <karini.magalhaes@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-10-05 16:31:31 +0000

    Acesso ao menu e pesquisa Tipo Base Legal.

---
+++ src/br/com/maph/selenium/tipoBaseLegal/PesquisarTipoBaseLegal.java
@@ -0,0 +1,82 @@
+package br.com.maph.selenium.tipoBaseLegal;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import br.com.marph.selenium.conexao.Conexao;
+
+public class PesquisarTipoBaseLegal {
+	private final String LOG_NAME = "Karini";
+	private WebDriver driver;
+	private Logger log = LogManager.getLogger(LOG_NAME);
+
+	@Before
+	public void startBrowser(){
+		driver = new FirefoxDriver();
+		Conexao.ip(driver); 
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	}
+	
+	@Test 
+	public void pesquisaTipoBaseLegal(){
+		
+		log.info("Início do teste - pesquisar tipo de base legal");
+		
+		long timestart = System.currentTimeMillis();
+		
+		MenuTipoBaseLegalTemplate.prepararAcessoTipoBaseLegal(driver);
+		
+		pesquisar();
+		
+		float tempoGasto = (System.currentTimeMillis() - timestart );
+		float tempoSegundos = tempoGasto/1000;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Entrada no sistema - ").append(tempoSegundos).append(" segundos");
+	
+		if(tempoSegundos>5000){
+			log.warn(sb.toString()+"\n");
+		}else{
+			log.info(sb.toString()+"\n");
+		}
+	}
+	
+	public void pesquisar(){
+		WebElement nome = driver.findElement(By.id("nome"));
+		nome.sendKeys("Nota Técnica");
+		
+		WebElement pesqAvancada = driver.findElement(By.id("btnExpandirPesquisaAvancada"));
+		pesqAvancada.click();
+		
+		WebElement transfRecursos = driver.findElement(By.id("transferenciaRecursosFinanceiros_chosen"));
+		transfRecursos.click();
+		
+		WebElement transSeleciona = driver.findElement(By.xpath("//li[@data-option-array-index='1']"));
+		transSeleciona.click();
+		
+		WebElement prestacaoMetas = driver.findElement(By.id("prestacaoMetas_chosen"));
+		prestacaoMetas.click();
+		
+		WebElement prestacaoMetasSeleciona = driver.findElement(By.xpath("//*[@id='prestacaoMetas_chosen']/div/ul/li[1]"));
+		prestacaoMetasSeleciona.click();
+		
+		WebElement prestacaoContas = driver.findElement(By.id("prestacaoContas_chosen"));
+		prestacaoContas.click();
+		
+		WebElement prestacaoContasSeleciona = driver.findElement(By.xpath("//*[@id='prestacaoContas_chosen']/div/ul/li[2]"));
+		prestacaoContasSeleciona.click();
+		
+		WebElement btnPesquisar = driver.findElement(By.id("btnPesquisar"));
+		btnPesquisar.click();
+		
+	}
+}
