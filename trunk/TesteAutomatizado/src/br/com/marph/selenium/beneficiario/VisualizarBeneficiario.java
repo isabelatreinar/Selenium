0752c19b5a479b7dc8661fbf16618d757a404452
karini.magalhaes <karini.magalhaes@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-10-06 16:45:23 +0000

    Visualizar beneficiário e responsável legal.

---
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/beneficiario/VisualizarBeneficiario.java
@@ -0,0 +1,63 @@
+package br.com.marph.selenium.beneficiario;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import br.com.marph.selenium.conexao.Conexao;
+
+public class VisualizarBeneficiario {
+	private final String LOG_NAME = "Karini";
+	private WebDriver driver;
+	private Logger log = LogManager.getLogger(LOG_NAME);	
+	
+	@Before
+	public void startBrowser(){
+		driver = new FirefoxDriver();
+		Conexao.ip(driver);  
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void visualizarResponsavel(){
+		
+		log.info("Início do teste Visualizar beneficiário");
+		
+		long timestart = System.currentTimeMillis();
+		
+		//Acessa Menu Cadastros Beneficiário
+		MenuBeneficiarioTemplate.prepararAcessoBaseLegal(driver);
+		
+		// Pesquisa um beneficiário na base de dados
+		PesquisarBeneficiarioMozilla.pesquisar(driver);
+		
+		// visualizar beneficiario
+		visualizar(driver);
+		
+		float tempoGasto = (System.currentTimeMillis() - timestart );
+		float tempoSegundos = tempoGasto/1000;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Entrada no sistema - ").append(tempoSegundos).append(" segundos");
+	
+		if(tempoSegundos>5000){
+			log.warn(sb.toString()+"\n");
+		}else{
+			log.info(sb.toString()+"\n");
+		}
+	}
+	
+	public static void visualizar(WebDriver driver){
+		WebElement beneficiario = driver.findElement(By.xpath("//td[@class='sorting_1']"));
+		beneficiario.click();
+		
+	}
+}
---
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/beneficiario/VisualizarResponsavel.java
@@ -0,0 +1,68 @@
+package br.com.marph.selenium.beneficiario;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import br.com.marph.selenium.conexao.Conexao;
+
+public class VisualizarResponsavel {
+	private final String LOG_NAME = "RAFAEL";
+	private WebDriver driver;
+	private Logger log = LogManager.getLogger(LOG_NAME);	
+	
+	@Before
+	public void startBrowser(){
+		driver = new FirefoxDriver();
+		Conexao.ip(driver);  
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void visualizarResponsavel(){
+		
+		log.info("Início do teste Visualizar responsável legal de beneficiário");
+		
+		long timestart = System.currentTimeMillis();
+		
+		//Acessa Menu Cadastros Beneficiário
+		MenuBeneficiarioTemplate.prepararAcessoBaseLegal(driver);
+		
+		// Pesquisa um beneficiário na base de dados
+		PesquisarBeneficiarioMozilla.pesquisar(driver);
+		
+		//Visualizar beneficiario
+		VisualizarBeneficiario.visualizar(driver);
+		
+		//Visualizar responsável legal
+		visualizar();
+		
+		float tempoGasto = (System.currentTimeMillis() - timestart );
+		float tempoSegundos = tempoGasto/1000;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Entrada no sistema - ").append(tempoSegundos).append(" segundos");
+	
+		if(tempoSegundos>5000){
+			log.warn(sb.toString()+"\n");
+		}else{
+			log.info(sb.toString()+"\n");
+		}
+	}
+
+	private void visualizar() {
+		// clica no botão de responsável legal
+		WebElement btnResponsavel = driver.findElement(By.id("btnPerfil1"));
+		btnResponsavel.click();
+	}
+	
+	
+}
