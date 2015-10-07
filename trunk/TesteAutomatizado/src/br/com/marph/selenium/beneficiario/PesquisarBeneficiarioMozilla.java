e5754e53970b3ea64acd461cd6431a511923953e
karini.magalhaes <karini.magalhaes@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-10-07 18:04:15 +0000

    Pesquisa utilizada no teste de validação da tela de beneficiário.

--- trunk/TesteAutomatizado/src/br/com/marph/selenium/beneficiario/PesquisarBeneficiarioMozilla.java
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/beneficiario/PesquisarBeneficiarioMozilla.java
@@ -1,56 +1,10 @@
 package br.com.marph.selenium.beneficiario;
 
-import java.util.concurrent.TimeUnit;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import br.com.maph.selenium.enums.EnumMensagens;
-import br.com.marph.selenium.conexao.Conexao;
-import br.com.marph.selenium.utils.LogUtils;
 
 public class PesquisarBeneficiarioMozilla {
-	private final String LOG_NAME = "RAFAEL";
-	private WebDriver driver;
-	private Logger log = LogManager.getLogger(LOG_NAME);	
-	
-	@Before
-	public void startBrowser(){
-		driver = new FirefoxDriver();
-		Conexao.ip(driver);  
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		}	
-
-	@Test
-	public void realizaCadastro(){			
-		
-		LogUtils.log(EnumMensagens.INICIO, this.getClass());
-		
-		long timestart = System.currentTimeMillis();		
-		
-		MenuBeneficiarioTemplate.prepararAcessoBaseLegal(driver);	
-	
-		pesquisar(driver);	
-		
-		float tempoGasto = (System.currentTimeMillis() - timestart );
-		float tempoSegundos = tempoGasto/1000;
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("Entrada no sistema - ").append(tempoSegundos).append(" segundos - FINALIZADO COM SUCESSO\n");
-	
-		if(tempoSegundos>5000){
-			log.warn(sb.toString()+"\n");
-		}else{
-			log.info(sb.toString()+"\n");
-		}	
-	}
 
 	public static void pesquisar(WebDriver driver) {
 		WebElement nome = driver.findElement(By.id("buscaNome"));
