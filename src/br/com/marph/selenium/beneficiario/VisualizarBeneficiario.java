0924bce5fb6d7c37d729ce2cdb6292bb66a7054e
karini.magalhaes <karini.magalhaes@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-10-07 18:00:46 +0000

    Inserir nome de usuário da máquina no log.

--- src/br/com/marph/selenium/beneficiario/VisualizarBeneficiario.java
+++ src/br/com/marph/selenium/beneficiario/VisualizarBeneficiario.java
@@ -11,69 +11,69 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import br.com.maph.selenium.enums.EnumMensagens;
 import br.com.marph.selenium.conexao.Conexao;
+import br.com.marph.selenium.exceptions.TesteAutomatizadoException;
+import br.com.marph.selenium.utils.LogUtils;
 
 public class VisualizarBeneficiario {
-	private final String LOG_NAME = "RAFAEL";
+	private final String LOG_NAME = System.getProperty("user.name");
 	private WebDriver driver;
-	private Logger log = LogManager.getLogger(LOG_NAME);	
-	
+	private Logger log = LogManager.getLogger(LOG_NAME);
+
+	public String beneficiarioSelecionado;
+
 	@Before
-	public void startBrowser(){
+	public void startBrowser() {
 		driver = new FirefoxDriver();
-		Conexao.ip(driver);  
+		Conexao.ip(driver);
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
-	
+
 	@Test
-	public void visualizarResponsavel(){
-		
-		log.info("Início do teste Visualizar beneficiário");
-		
+	public void visualizarBeneficiario() throws TesteAutomatizadoException {
+
+		LogUtils.log(EnumMensagens.INICIO, this.getClass());
 		long timestart = System.currentTimeMillis();
-		
-		//Acessa Menu Cadastros Beneficiário
+
+		// Acessa menu
 		MenuBeneficiarioTemplate.prepararAcessoBaseLegal(driver);
-		
+
 		// Pesquisa um beneficiário na base de dados
 		PesquisarBeneficiarioMozilla.pesquisar(driver);
-		
+
 		// visualizar beneficiario
 		visualizar(driver);
-		
-		//teste botão voltar superior
-		voltarSuperior();
-		
-		// teste botão voltar inferior
-		visualizar(driver);
-		voltarInferior();
-		
-		float tempoGasto = (System.currentTimeMillis() - timestart );
-		float tempoSegundos = tempoGasto/1000;
-		
+
+		// valida se a tela acessada é a correta
+		if (!driver.findElement(By.id("gridSystemModalLabel")).getText().equalsIgnoreCase("Visualizar beneficiário")) {
+			throw new TesteAutomatizadoException(EnumMensagens.TELA_INCORRETA, this.getClass());
+		}
+		// se a tela é a correta -> verifica se é do beneficiario correto
+		if (!beneficiarioSelecionado.equalsIgnoreCase(driver.findElement(By.id("modalVisualizarNome")).getText())) {
+			throw new TesteAutomatizadoException(EnumMensagens.BENEFICIARIO_INCORRETO, this.getClass());
+		}
+
+		// Se for a correta o teste se encerra
+		float tempoGasto = (System.currentTimeMillis() - timestart);
+		float tempoSegundos = tempoGasto / 1000;
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("Entrada no sistema - ").append(tempoSegundos).append(" segundos - FINALIZADO COM SUCESSO\n");
-	
-		if(tempoSegundos>5000){
-			log.warn(sb.toString()+"\n");
-		}else{
-			log.info(sb.toString()+"\n");
+		
+
+		if (tempoSegundos > 5000) {
+			log.warn(sb.toString() + "\n");
+		} else {
+			log.info(sb.toString() + "\n");
 		}
 	}
-	
-	public static void visualizar(WebDriver driver){
+
+	public void visualizar(WebDriver driver) {
 		WebElement beneficiario = driver.findElement(By.xpath("//td[@class='sorting_1']"));
+		beneficiarioSelecionado = beneficiario.getText();
 		beneficiario.click();
-	}
-	
-	public void voltarSuperior(){
-		WebElement btnVoltar = driver.findElement(By.id("btnVoltarBarraTarefas"));
-		btnVoltar.click();
-	}
-	
-	public void voltarInferior(){
-		WebElement btnVoltar = driver.findElement(By.id("btnVoltar"));
-		btnVoltar.click();
+		
 	}
 }
