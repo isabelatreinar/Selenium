3cc0d878ea672e704e6c3c9dbb0930eafbe4c8e2
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-10-15 20:26:09 +0000

    Cadastrar resolução criado.

--- src/br/com/marph/selenium/resolucao/CadastroResolucao.java
+++ src/br/com/marph/selenium/resolucao/CadastroResolucao.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -40,7 +41,16 @@ public class CadastroResolucao {
 		
 		cadastrarResolucao();
 		
-
+		WebElement avancar = driver.findElement(By.id("btnProximo"));
+		avancar.click();
+		
+		beneficiarios();
+		
+		WebElement avancar1 = driver.findElement(By.id("btnProximo"));
+		avancar1.click();
+		
+		indicadores();
+		
 		float tempoGasto = (System.currentTimeMillis() - timestart );
 		float tempoSegundos = tempoGasto/1000;
 		
@@ -54,28 +64,65 @@ public class CadastroResolucao {
 		}		
 	}
 
+	protected void indicadores(){
+		
+		WebElement criar = driver.findElement(By.id("criar"));
+		criar.click();
+		
+		WebElement nome = driver.findElement(By.id("nome"));
+		nome.sendKeys("Teste");
+		
+		WebElement btnIndicador = driver.findElement(By.xpath("//*[@class='_cadastroIndicador']"));
+		btnIndicador.click();
+		
+		WebElement indicador = driver.findElement(By.xpath("//*[@data-label-field='descricao']"));
+		indicador.sendKeys("Taxa");
+		WebElement indicador1 = driver.findElement(By.id("ui-id-2"));
+		indicador1.click();
+		
+		WebElement ponto = driver.findElement(By.xpath("//*[@id='tabelaIndicadoresNovo']/div[2]/div[3]/input"));
+		ponto.sendKeys("5000");
+		
+		WebElement peso = driver.findElement(By.xpath("//*[@id='tabelaIndicadoresNovo']/div[2]/div[4]/input"));
+		peso.sendKeys("10000");
+		
+		WebElement preRequisito = driver.findElement(By.xpath("//*[@id='tabelaIndicadoresNovo']/div[2]/div[5]/a"));
+		preRequisito.click();
+		
+		WebElement salvar = driver.findElement(By.xpath("//*[@id='headingNovo']/ul/li[1]/a"));
+		salvar.click();
+		
+	}
+
+	protected void beneficiarios() {
+		WebElement upload = driver.findElement(By.id("uploadBeneficiariosContemplados"));
+		upload.sendKeys("C:\\Users\\rafael.sad\\Documents\\beneficiarioExport.xls");
+		
+		WebElement importar = driver.findElement(By.id("buttonImportar"));
+		importar.click();
+	}
+
 	private void cadastrarResolucao() {
 		WebElement cadastrarResolucao = driver.findElement(By.id("btnNovaResolucao"));
 		cadastrarResolucao.click();
 		//Selecionar programa
 		WebElement programaSelecionar = driver.findElement(By.id("programa_chosen"));
-		programaSelecionar.click();
-		
-		WebElement programa = driver.findElement(By.xpath("//li[@data-option-array-index='3']"));
-		programa.click();		
+		programaSelecionar.click();		
+		WebElement programa = driver.findElement(By.xpath("//*[@id='programa_chosen']/div/div/input"));
+		programa.sendKeys("farmácia de minas");
+		programa.sendKeys(Keys.TAB);
 		//fim
+		
 		//numero resolucao
 		WebElement numero = driver.findElement(By.id("baseLegal"));
-		numero.sendKeys("456");
-		
-		WebElement numeroSeleciona = driver.findElement(By.xpath("//li[@id='ui-id-6']"));
+		numero.sendKeys("123");		
+		WebElement numeroSeleciona = driver.findElement(By.xpath("//li[@id='ui-id-2']"));
 		numeroSeleciona.click();
 		//fim
 		//base legal
 		WebElement selecionarBase = driver.findElement(By.id("termosBaseLegal_chosen"));
-		selecionarBase.click();
-		
-		WebElement base = driver.findElement(By.xpath("//*[@id='termosBaseLegal_chosen']/div/ul/li[3]"));
+		selecionarBase.click();		
+		WebElement base = driver.findElement(By.xpath("//*[@id='termosBaseLegal_chosen']/div/ul/li[2]"));
 		base.click();
 		//fim
 		WebElement tempo = driver.findElement(By.id("tempoVigencia"));
