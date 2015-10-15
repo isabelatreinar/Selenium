34aba4da6f408dba807319591b3acea12dbf7903
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-10-15 20:58:05 +0000

    criação de classe e pacote Acessibilidade.

---
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/acessibilidade/TestarAcessibilidade.java
@@ -0,0 +1,67 @@
+package br.com.marph.selenium.acessibilidade;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import br.com.marph.selenium.conexao.Conexao;
+import br.com.marph.selenium.enums.EnumMensagens;
+import br.com.marph.selenium.exceptions.TesteAutomatizadoException;
+import br.com.marph.selenium.usuario.MenuUsuarioTemplate;
+import br.com.marph.selenium.utils.LogUtils;
+
+public class TestarAcessibilidade {
+	private final String LOG_NAME = System.getProperty("user.name");
+	private WebDriver driver;
+	private Logger log = LogManager.getLogger(LOG_NAME);
+
+	@Before
+	public void startBrowser() {
+		driver = new FirefoxDriver();
+		Conexao.ip(driver);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TestarAcessibilidadePagina() throws TesteAutomatizadoException {
+
+		LogUtils.log(EnumMensagens.INICIO, this.getClass());
+		long timestart = System.currentTimeMillis();
+
+		// Acessar menu
+		MenuUsuarioTemplate.prepararAcessoUsuario(driver);
+
+		WebElement aumentar = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/ul/li[4]/a"));
+		for (int i = 0; i <= 8; i++) {
+			aumentar.click();
+		}
+		
+		WebElement diminuir = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/ul/li[2]/a"));
+		for (int i = 0; i <= 3; i++) {
+			diminuir.click();
+		}
+		
+		WebElement original = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/ul/li[3]/a"));
+		original.click();
+
+		float tempoGasto = (System.currentTimeMillis() - timestart);
+		float tempoSegundos = tempoGasto / 1000;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("Entrada no sistema - ").append(tempoSegundos).append(" segundos - FINALIZADO COM SUCESSO\n");
+
+		if (tempoSegundos > 5000) {
+			log.warn(sb.toString() + "\n");
+		} else {
+			log.info(sb.toString() + "\n");
+		}
+	}
+}
