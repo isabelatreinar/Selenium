89c4be2876197c5635e303dbce6515fb7fcebb8c
karini.magalhaes <karini.magalhaes@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-10-23 15:29:21 +0000

    Padronização do nome da classe.

--- trunk/TesteAutomatizado/src/br/com/marph/selenium/beneficiario/CancelarEdicaoBeneficiario.java
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/beneficiario/CancelarEdicaoBeneficiario.java
@@ -6,7 +6,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
