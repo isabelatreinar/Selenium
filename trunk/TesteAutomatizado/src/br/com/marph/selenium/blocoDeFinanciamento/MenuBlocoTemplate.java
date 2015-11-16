2c8726a5893d1e806f684b7f6a064685b5c837ac
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-11-16 19:33:47 +0000

    Criação das classes cadastrar bloco de financiamento, editar,pesquisar e visualizar.

---
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/blocoDeFinanciamento/MenuBlocoTemplate.java
@@ -0,0 +1,30 @@
+package br.com.marph.selenium.blocoDeFinanciamento;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MenuBlocoTemplate {
+	public static void prepararAcessoBloco(WebDriver driver) {
+		WebElement fecharbtn = driver.findElement(By.id("closeModalHome"));
+		fecharbtn.click();
+		
+		WebElement btnEntrar = driver.findElement(By.id("btnEntradaSistemaID"));
+		btnEntrar.click();
+		
+		WebElement btnAcessar = driver.findElement(By.id("btnAcessar"));
+		btnAcessar.click();
+		
+		WebElement btnConfirmar = driver.findElement(By.id("confirmarDados"));
+		btnConfirmar.click();
+		
+		WebElement btnAcessarSist = driver.findElement(By.id("acessarSistema"));
+		btnAcessarSist.click();			
+		
+		WebElement menuCadastrar = driver.findElement(By.xpath("//td[@onmouseup='cmItemMouseUp (this,2)']"));
+		menuCadastrar.click(); 
+		
+		WebElement menuUsuario = driver.findElement(By.xpath("//*[@id='blocoFinanciamentoMenu']"));
+		menuUsuario.click();
+	}
+}
