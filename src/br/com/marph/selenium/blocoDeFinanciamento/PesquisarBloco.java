a3c888ee1340466269f450f0bdc98cdf63491af7
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-11-16 19:33:47 +0000

    Criação das classes cadastrar bloco de financiamento, editar,pesquisar e visualizar.

---
+++ src/br/com/marph/selenium/blocoDeFinanciamento/PesquisarBloco.java
@@ -0,0 +1,68 @@
+package br.com.marph.selenium.blocoDeFinanciamento;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import br.com.marph.selenium.conexao.Conexao;
+import br.com.marph.selenium.enums.EnumMensagens;
+import br.com.marph.selenium.exceptions.TesteAutomatizadoException;
+import br.com.marph.selenium.utils.LogUtils;
+
+public class PesquisarBloco {
+	private final String LOG_NAME = System.getProperty("user.name");
+	private WebDriver driver;
+	private Logger log = LogManager.getLogger(LOG_NAME);
+	
+	@Before
+	public void startBrowser() {
+		driver = new FirefoxDriver();
+		Conexao.ip(driver);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void realizaBusca() throws InterruptedException, TesteAutomatizadoException {
+
+		LogUtils.log(EnumMensagens.INICIO, this.getClass());
+
+		long timestart = System.currentTimeMillis();
+
+		MenuBlocoTemplate.prepararAcessoBloco(driver);
+		
+		pesquisar(driver);
+		
+		float tempoGasto = (System.currentTimeMillis() - timestart);
+		float tempoSegundos = tempoGasto / 1000;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("Entrada no sistema - ").append(tempoSegundos).append(" segundos");
+
+		if (tempoSegundos > 5000) {
+			log.warn(sb.toString() + "\n");
+		} else {
+			log.info(sb.toString() + "\n");
+		}
+	}
+	
+	public static void pesquisar(WebDriver driver) {
+		//insere o nome a ser pesquisado
+		driver.findElement(By.id("nome")).sendKeys("marph");
+		
+		//situação
+		driver.findElement(By.id("situacao_chosen")).click();
+		driver.findElement(By.xpath("//*[@id='situacao_chosen']/div/div/input")).sendKeys("Ativo");
+		driver.findElement(By.xpath("//*[@id='situacao_chosen']/div/div/input")).sendKeys(Keys.TAB);
+		
+		//clica em pesquisar
+		driver.findElement(By.id("btnPesquisar")).click();
+	}	
+}
