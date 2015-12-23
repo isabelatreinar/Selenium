f1c20411ff957e64fca0779eb10b56cac63b2072
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2015-12-23 15:30:46 +0000

    Atualizado versao do driver do selenium

--- trunk/TesteAutomatizado/src/br/com/marph/selenium/testesExcell/BaseLegalExcell.java
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/testesExcell/BaseLegalExcell.java
@@ -55,12 +55,11 @@ public class BaseLegalExcell {
 				.equalsIgnoreCase("Você está em: Base Legal > Nova Base Legal")) {
 			validar();
 		}
-		
-		if (! driver.findElement(By.xpath("//ol[@class='breadcrumb small']")).getText()
+
+		if (!driver.findElement(By.xpath("//ol[@class='breadcrumb small']")).getText()
 				.equalsIgnoreCase("Você está em: Base Legal > Nova Base Legal")) {
 			ValidaToast.valida(driver);
 		}
-		
 
 		float tempoGasto = (System.currentTimeMillis() - timestart);
 		float tempoSegundos = tempoGasto / 1000;
@@ -74,71 +73,68 @@ public class BaseLegalExcell {
 		}
 	}
 
-	
-
 	protected void validar() throws TesteAutomatizadoException {
 		try {
 			driver.findElement(By.id("numero")).click();
 			if ("Existe tipo de base legal cadastrado com esse número"
 					.equalsIgnoreCase(driver.findElement(By.xpath("//*[@id='numero_maindiv']/div")).getText())) {
 				throw new TesteAutomatizadoException(EnumMensagens.BASE_LEGAL_JA_CADASTRADA, this.getClass());
-			}else{
+			} else {
 				throw new TesteAutomatizadoException(EnumMensagens.PDF_MAIOR, this.getClass());
-			}			
+			}
 		} catch (NoSuchElementException e) {
-			
-		}	
+
+		}
 	}
 
 	protected void cadastro() throws TesteAutomatizadoException {
-		
-			WorkbookSettings workbookSettings = new WorkbookSettings();
-			workbookSettings.setEncoding("ISO-8859-1");
-			Workbook wb = null;
-			try {
-				wb = Workbook.getWorkbook(new File("./data/baseLegal.xls"), workbookSettings);
-			} catch (BiffException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			Sheet sheet = wb.getSheet(0);
-			String tipo = sheet.getCell(0, 1).getContents();
-			String numero = sheet.getCell(1, 1).getContents();
-			String data = sheet.getCell(2, 1).getContents();
-			String ano = sheet.getCell(3, 1).getContents();
-			String pdf = sheet.getCell(4, 1).getContents();
 
-			if (StringUtils.isNotBlank(tipo)) {
-				driver.findElement(By.id("tipoBaseLegal_chosen")).click();
-				driver.findElement(By.xpath("//*[@id='tipoBaseLegal_chosen']/div/div/input")).sendKeys(tipo);
-				driver.findElement(By.xpath("//*[@id='tipoBaseLegal_chosen']/div/div/input")).sendKeys(Keys.TAB);
-			} else
-				throw new TesteAutomatizadoException(EnumMensagens.TIPO_EM_BRANCO, this.getClass());
+		WorkbookSettings workbookSettings = new WorkbookSettings();
+		workbookSettings.setEncoding("ISO-8859-1");
+		Workbook wb = null;
+		try {
+			wb = Workbook.getWorkbook(new File("./data/baseLegal.xls"), workbookSettings);
+		} catch (BiffException | IOException e) {
 
-			if (StringUtils.isNotBlank(numero)) {
-				driver.findElement(By.id("numero")).sendKeys(numero);
-			} else
-				throw new TesteAutomatizadoException(EnumMensagens.NUMERO_EM_BRANCO, this.getClass());
+			e.printStackTrace();
+		}
+		Sheet sheet = wb.getSheet(0);
+		String tipo = sheet.getCell(0, 1).getContents();
+		String numero = sheet.getCell(1, 1).getContents();
+		String data = sheet.getCell(2, 1).getContents();
+		String ano = sheet.getCell(3, 1).getContents();
+		String pdf = sheet.getCell(4, 1).getContents();
 
-			if (StringUtils.isNotBlank(data)) {
-				driver.findElement(By.id("dataPublicacao")).sendKeys(data);
-			} else
-				throw new TesteAutomatizadoException(EnumMensagens.DATA_EM_BRANCO, this.getClass());
+		if (StringUtils.isNotBlank(tipo)) {
+			driver.findElement(By.id("tipoBaseLegal_chosen")).click();
+			driver.findElement(By.xpath("//*[@id='tipoBaseLegal_chosen']/div/div/input")).sendKeys(tipo);
+			driver.findElement(By.xpath("//*[@id='tipoBaseLegal_chosen']/div/div/input")).sendKeys(Keys.TAB);
+		} else
+			throw new TesteAutomatizadoException(EnumMensagens.TIPO_EM_BRANCO, this.getClass());
 
-			if (StringUtils.isNotBlank(ano)) {
-				driver.findElement(By.id("dataVigencia_chosen")).click();
-				driver.findElement(By.xpath("//*[@id='dataVigencia_chosen']/div/div/input")).sendKeys(ano);
-				driver.findElement(By.xpath("//*[@id='dataVigencia_chosen']/div/div/input")).sendKeys(Keys.TAB);
-			} else
-				throw new TesteAutomatizadoException(EnumMensagens.ANO_EM_BRANCO, this.getClass());
+		if (StringUtils.isNotBlank(numero)) {
+			driver.findElement(By.id("numero")).sendKeys(numero);
+		} else
+			throw new TesteAutomatizadoException(EnumMensagens.NUMERO_EM_BRANCO, this.getClass());
 
-			if (StringUtils.isNotBlank(pdf)) {
-				driver.findElement(By.id("textoPublicado")).sendKeys(pdf);
-			} else
-				throw new TesteAutomatizadoException(EnumMensagens.PDF_EM_BRANCO, this.getClass());
+		if (StringUtils.isNotBlank(data)) {
+			driver.findElement(By.id("dataPublicacao")).sendKeys(data);
+		} else
+			throw new TesteAutomatizadoException(EnumMensagens.DATA_EM_BRANCO, this.getClass());
 
-			driver.findElement(By.id("btnSalvar")).click();
+		if (StringUtils.isNotBlank(ano)) {
+			driver.findElement(By.id("dataVigencia_chosen")).click();
+			driver.findElement(By.xpath("//*[@id='dataVigencia_chosen']/div/div/input")).sendKeys(ano);
+			driver.findElement(By.xpath("//*[@id='dataVigencia_chosen']/div/div/input")).sendKeys(Keys.TAB);
+		} else
+			throw new TesteAutomatizadoException(EnumMensagens.ANO_EM_BRANCO, this.getClass());
 
-		
+		if (StringUtils.isNotBlank(pdf)) {
+			driver.findElement(By.id("textoPublicado")).sendKeys(pdf);
+		} else
+			throw new TesteAutomatizadoException(EnumMensagens.PDF_EM_BRANCO, this.getClass());
+
+		driver.findElement(By.id("btnSalvar")).click();
+
 	}
 }
