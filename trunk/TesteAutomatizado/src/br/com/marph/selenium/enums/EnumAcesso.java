e7d83d1a7db0db7fce8d61739decf2e4674947fe
rafael.sad <rafael.sad@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2016-02-22 17:35:20 +0000

    Resolução funcionando completa e algumas alterações em subsecretaria e nas planilhas.

--- trunk/TesteAutomatizado/src/br/com/marph/selenium/enums/EnumAcesso.java
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/enums/EnumAcesso.java
@@ -1,8 +1,8 @@
 package br.com.marph.selenium.enums;
 
 public enum EnumAcesso {
-	ADMINISTRADOR("17069");
-	
+	ADMINISTRADOR("18071");
+	// 18071 porta 8081
 	private String id;
 
 	private EnumAcesso(String id) {
