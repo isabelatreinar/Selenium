374efdc7fde098ea027958602b9c81a5a96f44f0
isabela_oliveira <isabela_oliveira@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2016-07-18 17:14:51 +0000

    Ultimas alteraçoes

--- trunk/TesteAutomatizado/src/br/com/marph/selenium/conexao/Conexao.java
+++ trunk/TesteAutomatizado/src/br/com/marph/selenium/conexao/Conexao.java
@@ -5,6 +5,6 @@ import org.openqa.selenium.WebDriver;
 public class Conexao {
 	
 	public static void ip(WebDriver driver){
-		driver.get("http://172.16.10.115:8080");
+		driver.get("http://172.16.10.115:8080/public/index");
 	}
 }
